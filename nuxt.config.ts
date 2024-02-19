954c9eb72a962b1d575154ec33c6f14260d08b08
hiep pham <hiep.pham@tomosia.com>
2024-02-19 14:08:37 +0700

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@ant-design-vue/nuxt', '@nuxtjs/tailwindcss'],
+  alias: {
+    '@ant-design/vue': 'node_modules/ant-design-vue/es',
+  },
 })
